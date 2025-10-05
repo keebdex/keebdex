fdbda4918c01380ca41014e694d27c234861b42f
Anh Thang Bui <buianhthang89@gmail.com>
2025-10-05 12:03:57 +0700

    feat: add detailed inventory drawer to marketplace view

--- app/components/MarketplaceListing.vue
+++ app/components/MarketplaceListing.vue
@@ -1,37 +1,88 @@
 <template>
   <UPageCard variant="outline" :ui="{ container: 'divide-y divide-default' }">
-    <UPageCard
+    <USlideover
       v-for="trade in trades"
       :key="trade.id"
-      variant="naked"
-      :title="trade.contact"
-      :description="trade.message"
-      :icon="
-        trade.intent === 'want'
-          ? 'hugeicons:money-bag-02'
-          : 'hugeicons:sale-tag-02'
-      "
-      :ui="{
-        leadingIcon: trade.intent === 'want' ? 'text-info' : 'text-warning',
-      }"
-      orientation="horizontal"
-      class="flex items-center justify-between not-last:pb-4 gap-2"
+      title="Trade Inventory"
+      description="All items listed are available and ready for your trade proposal."
     >
-      <UAvatarGroup size="xl" :max="15">
-        <UTooltip
-          v-for="{ id, artisan } in trade.items"
-          :key="id"
-          :delay-duration="0"
-          :text="colorwayTitle(artisan)"
-        >
-          <UAvatar :src="artisan.img" :alt="artisan.name" />
-        </UTooltip>
-      </UAvatarGroup>
-    </UPageCard>
+      <UPageCard
+        variant="naked"
+        :title="trade.contact"
+        :description="trade.message"
+        :icon="
+          trade.intent === 'want'
+            ? 'hugeicons:money-bag-02'
+            : 'hugeicons:sale-tag-02'
+        "
+        :ui="{
+          root: 'cursor-pointer',
+          leadingIcon: trade.intent === 'want' ? 'text-info' : 'text-warning',
+        }"
+        orientation="horizontal"
+        class="flex items-center justify-between not-last:pb-4 gap-2"
+      >
+        <UAvatarGroup size="xl" :max="15">
+          <UTooltip
+            v-for="{ id, artisan } in trade.items"
+            :key="id"
+            :delay-duration="0"
+            :text="colorwayTitle(artisan)"
+          >
+            <UAvatar :src="artisan.img" :alt="artisan.name" />
+          </UTooltip>
+        </UAvatarGroup>
+      </UPageCard>
+
+      <template #body>
+        <UPageHeader
+          :headline="trade.intent === 'want' ? 'Buying' : 'Selling'"
+          :title="trade.contact"
+          :description="trade.message"
+        />
+
+        <UPageGrid class="grid !grid-cols-2 gap-4 mt-4">
+          <UPageCard
+            v-for="{ id, artisan } in trade.items"
+            :key="id"
+            :title="artisan.name"
+            :description="artisan?.sculpt.name"
+            orientation="vertical"
+            reverse
+            :ui="{
+              root: 'h-full',
+              wrapper: 'flex-1',
+            }"
+            variant="soft"
+          >
+            <div class="aspect-square overflow-hidden">
+              <NuxtImg
+                loading="lazy"
+                :alt="artisan.name"
+                :src="artisan.img"
+                class="w-full h-full object-cover rounded"
+              />
+            </div>
+          </UPageCard>
+        </UPageGrid>
+      </template>
+
+      <template #footer>
+        <UButton
+          block
+          label="Copy Discord"
+          icon="hugeicons:discord"
+          color="info"
+          @click="copyToClipboard(trade.contact)"
+        />
+      </template>
+    </USlideover>
   </UPageCard>
 </template>
 
 <script setup>
+const toast = useToast()
+
 const marketplaceCfg = useState('marketplace-config', () => ({
   type: 'any',
 }))
@@ -55,20 +106,11 @@ const trades = computed(() => {
   return type === 'any' ? data.value : data.value.filter((i) => i.type === type)
 })
 
-// const chunkSize = 12
-// const firstChunk = (items) => {
-//   return items.length > chunkSize ? items.slice(0, chunkSize - 1) : items
-// }
-
-// const chunk = (array) => {
-//   const chunkedArray = []
-//   let index = 0
-
-//   while (index < array.length) {
-//     chunkedArray.push(array.slice(index, index + chunkSize))
-//     index += chunkSize
-//   }
-
-//   return chunkedArray
-// }
+const copyToClipboard = (discord) => {
+  navigator.clipboard.writeText(discord)
+  toast.add({
+    color: 'success',
+    title: 'Copied to clipboard!',
+  })
+}
 </script>
