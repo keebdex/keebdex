940a8fa0741e84f7e2727938d209ad8b48db6715
Anh Thang Bui <buianhthang89@gmail.com>
2026-06-24 17:39:14 +0700

    fix: change banner title

--- app/components/AppBanner.vue
+++ app/components/AppBanner.vue
@@ -1,12 +1,12 @@
 <template>
   <UBanner
     id="how-to-use-collection"
-    title="How to Use the Collection"
+    title="Manage Your Collection & Generate Wishlists"
     icon="hugeicons:collections-bookmark"
     color="info"
     :actions="[
       {
-        label: 'Watch Video',
+        label: 'Watch',
         to: 'https://www.loom.com/share/660fe9bd026640788b2d0d8a6fe9b6b8',
         trailingIcon: 'hugeicons:arrow-right-02',
         ui: {
